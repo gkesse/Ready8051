12de36ec0fdae4e1e3bc85d8677b785cb659ee8f
Gerard KESSE <tiakagerard@hotmail.com>
2021-02-21 18:47:28 +0100

    Initial Commit

--- app/code/readyapp/src/manager/GButton.c
+++ app/code/readyapp/src/manager/GButton.c
@@ -4,23 +4,23 @@
 //===============================================
 static uchar g_button_port;
 static uchar g_button_pin;
-static GBit g_led_state;
+static GBit g_button_state;
 //===============================================
 void GButton_Init(uchar port, uchar pin) {
     g_button_port = port;
     g_button_pin = pin;
-    g_led_state = 1;
+    g_button_state = 1;
     GPort_Bit_Write(g_button_port, g_button_pin, 1);
 }
 //===============================================
 void GButton_Update() {
     GBit l_button_state = GPort_Bit_Read(g_button_port, g_button_pin);
     if(l_button_state == 0) {
-        g_led_state = !g_led_state;
+        g_button_state = !g_button_state;
     }
 }
 //===============================================
 GBit GButton_Get_State() {
-    return g_led_state;
+    return g_button_state;
 }
 //=============================================== 
