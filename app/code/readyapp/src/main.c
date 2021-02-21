f1c87cec0fee085b788bf141036031b4d4ba852d
Gerard KESSE <tiakagerard@hotmail.com>
2021-02-21 18:54:19 +0100

    Initial Commit

--- app/code/readyapp/src/main.c
+++ app/code/readyapp/src/main.c
@@ -4,7 +4,7 @@
 //=============================================== 
 void main() {
     GSch_Init();
-    GProcess_Init(Button);
+    GProcess_Init(Led_Blink);
     GSch_Start();
     while(1) {
         GSch_Dispatch_Tasks();
